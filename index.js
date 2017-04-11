37b863fd91a8bd58ad72e4cabde140ba1f6ce32b
Damian Dennis <damiandennis@gmail.com>
2017-04-12 07:22:44 +1000

    updated to 4.0.0

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 var end_point_service_1 = require("./lib/services/end-point.service");
 exports.EndPointService = end_point_service_1.EndPointService;
 var base_model_1 = require("./lib/models/base.model");
