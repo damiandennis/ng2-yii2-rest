c4247dc917b2ed0853bf931182e5077fec261213
Damian Dennis <damiandennis@gmail.com>
2017-06-20 06:36:03 +1000

    added error service.

---
+++ lib/services/error.service.d.ts
@@ -0,0 +1,44 @@
+import { Router } from "@angular/router";
+export declare class ErrorService {
+    router: Router;
+    loginRoute: string;
+    errorRoute: string;
+    forbiddenRoute: string;
+    criticalMessage: string;
+    forbiddenMessage: string;
+    private unAuthorisedMessage;
+    validationMessage: string;
+    constructor(router: Router);
+    /**
+     * This handles critical errors responses from server.
+     *
+     * @param error
+     */
+    handleCriticalResponse(error: any): void;
+    /**
+     * This handles validations errors that were not caught client side.
+     *
+     * @param error
+     */
+    handleValidationResponse(error: any): void;
+    /**
+     * Handles unauthorized access.
+     *
+     * @param error
+     */
+    handleForbiddenResponse(error: any): void;
+    /**
+     * Handles when some is required to be logged in but has not passed the router.
+     *
+     * @param error
+     */
+    handleUnAuthorisedResponse(error: any): void;
+    /**
+     * Generic error response.
+     *
+     * @param error
+     * @param useRouter
+     * @param lastLocation
+     */
+    response(error: any, useRouter?: boolean, lastLocation?: any): void;
+}
